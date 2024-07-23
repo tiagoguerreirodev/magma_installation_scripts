e2166b07c72d0efaafd47882e56565bc137c3beb
Tiago Guerreiro <49923133+tiagoguerreirodev@users.noreply.github.com>
2024-07-23 00:03:10 -0300

    Update agw_ubuntu_custom.sh

--- agw_ubuntu_custom.sh
+++ agw_ubuntu_custom.sh
@@ -65,7 +65,7 @@ INTERFACES=$(ip -br a)
 if [[ $1 != "$CLOUD_INSTALL" ]] && ( [[ ! $INTERFACES == *'eth0'*  ]] || [[ ! $INTERFACES == *'eth1'* ]] || ! grep -q 'GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"' /etc/default/grub); then
   # changing intefaces name
   sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
-  sed -i 's/enp0s3/eth0/g' /etc/netplan/50-cloud-init.yaml
+  # sed -i 's/enp0s3/eth0/g' /etc/netplan/50-cloud-init.yaml
   # changing interface name
   grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -75,9 +75,9 @@ if [[ $1 != "$CLOUD_INSTALL" ]] && ( [[ ! $INTERFACES == *'eth0'*  ]] || [[ ! $I
   service systemd-resolved restart
 
   # interface config
-  apt install -y ifupdown net-tools ipcalc
-  mkdir -p "$INTERFACE_DIR"
-  echo "source-directory $INTERFACE_DIR" > /etc/network/interfaces
+  # apt install -y ifupdown net-tools ipcalc
+  # mkdir -p "$INTERFACE_DIR"
+  # echo "source-directory $INTERFACE_DIR" > /etc/network/interfaces
 
   if [ -z "$addr1" ] || [ -z "$gw_addr" ]
   then
