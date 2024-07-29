a9d01dfd1353d65f119bcd4f684e1db3e4531747
Tiago Guerreiro <49923133+tiagoguerreirodev@users.noreply.github.com>
2024-07-29 15:45:09 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+chmod a+x ./install_prereqs.sh
+chmod a+x ./agw_ubuntu_custom.sh
+
+bash ./install_prereqs.sh
+
+source ~/.bashrc
+source ~/.profile
+
+cp ./eth0 /etc/network/interfaces.d/eth0
+cp ./eth1 /etc/network/interfaces.d/eth1
+
+ls /etc/network/interfaces.d
+sleep 5
+
+bash ./agw_ubuntu_custom.sh
