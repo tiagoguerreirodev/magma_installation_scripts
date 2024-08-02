0650fccc239770c4819d20b1298d5e47369bdece
Tiago Guerreiro <49923133+tiagoguerreirodev@users.noreply.github.com>
2024-08-02 15:15:06 -0300

    Update orc8r_prereqs.sh

--- orc8r_prereqs.sh
+++ orc8r_prereqs.sh
@@ -2,6 +2,10 @@ mkdir -p /var/opt/magma/configs/
 cp ./control_proxy.yml /var/opt/magma/configs/control_proxy.yml
 
 bash /home/magma/magma/orc8r/cloud/deploy/scripts/self_sign_certs.sh localhost
+bash /home/magma/magma/orc8r/cloud/deploy/scripts/create_application_certs.sh localhost
 
 mkdir -p /var/opt/magma/certs
+
 cp /home/magma/magma/orc8r/cloud/deploy/scripts/rootCA.pem /var/opt/magma/certs/rootCA.pem
+cp /home/magma/magma/orc8r/cloud/deploy/scripts/admin_operator.key.pem /var/opt/magma/certs/admin_operator.key.pem
+cp /home/magma/magma/orc8r/cloud/deploy/scripts/admin_operator.pem /var/opt/magma/certs/admin_operator.pem
