b31234c59461016970549ba298e64014b3780349
tiagoguerreirodev <tiago.guerreiro76@gmail.com>
2024-09-24 14:58:26 -0300

    Testing new Orc8r installation

---
+++ Docker-based installation/Orc8r/orc8r_installation.sh
@@ -0,0 +1,33 @@
+set -x
+
+cd /home/magma/magma/orc8r/cloud/docker
+
+sed -i 's@$PWD/../../../.cache/test_certs:/var/opt/magma/certs@/var/opt/magma/certs:/var/opt/magma/certs@g' /home/magma/magma/orc8r/cloud/docker/docker-compose.yml
+
+chmod a+x build.py
+python3 ./build.py --all
+
+chmod a+x run.py
+python3 ./run.py --metrics
+
+cd /home/magma/magma/nms
+
+sed -i '70,73d;39d' /home/magma/magma/nms/docker-compose.yml
+
+# sed -i 's/"8081:8081"/"8082:8081"/g' /home/magma/magma/nms/docker-compose.yml
+
+# COMPOSE_PROJECT_NAME=magmalte docker-compose build magmalte
+
+sed -i 's@API_CERT_FILENAME:-../.cache/test_certs/admin_operator.pem@API_CERT_FILENAME:-/var/opt/magma/certs/admin_operator.pem@g' /home/magma/magma/nms/docker-compose.yml
+sed -i 's@API_PRIVATE_KEY_FILENAME:-../.cache/test_certs/admin_operator.key.pem@API_CERT_FILENAME:-/var/opt/magma/certs/admin_operator.key.pem@g' /home/magma/magma/nms/docker-compose.yml
+
+docker-compose up -d
+
+sleep 10
+
+chmod a+x ./scripts/dev_setup.sh
+bash ./scripts/dev_setup.sh
+
+sed -i "1i127.0.0.1 magma\n127.0.0.1 magma-test\n127.0.0.1 bootstrapper-controller.magma.test\n127.0.0.1 controller.magma.test" /etc/hosts
+
+#TODO configure containers to auto-restart in reboot
